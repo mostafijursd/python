1a40e0018a966a5652b555944691f854e655786e
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-04-21 22:01:07 +0530

    second commit

---
+++ pythonbasic/data.py
@@ -0,0 +1,27 @@
+# a=2
+# b=5
+# c=a+b
+# print(c)
+# print(b>a)
+
+# user=input("this is name :")
+
+# print('welcome',user)
+
+# name=input('Name')
+# age=input('Age')
+# marks=input('Marks')
+
+# print( ':',name)
+# print( ':',age)
+# print( ':',marks)
+
+# a= int(input())
+# b=int(input())
+
+# c=a*b
+# print(c)
+
+user="This is Laptops . \n This laptop is poerfull"
+
+print(user)
---
+++ pythonbasic/string.py
@@ -0,0 +1,35 @@
+# # user="BABU"
+
+# # print(user[2:3])
+
+# # leng1="javascript"
+
+# # leng2="python"
+
+# # sum=leng1 + " " + leng2
+# # print(len(sum))
+
+# # print(sum[2:4])
+
+# # str1="javascript"
+
+# # print(str1[1:6])
+
+# # print(str1[-6:-1])
+
+# # string function 
+
+# str="I am student"
+
+# #print(str.endswith("ent"))
+
+# #print(str.replace("am","are"))
+# print(str.find('t'))
+
+
+age=20
+
+if age<18:
+    print("I AM READY ")
+else:
+    print(" you are not algabli")    
